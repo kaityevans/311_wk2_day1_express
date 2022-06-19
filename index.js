0c1db1bc3a698bd004921e5009cea209723a6a36
unknown <kaitlandevans@gmail.com>
2022-06-19 12:32:06 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+// app.use(bodyParser())
+app.use(express.json())
 const port = process.env.PORT || 4000
 
 const { users } = require('./state')
 
 /* BEGIN - create routes here */
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to my server</h1>')
+})
+app.get('/users', (req, res) => {
+  res.json(users)
+})
 
+ // the GET Method
+ app.get('/users/:id', (req, res) => {
+  console.log(req)
+  let id = req.params.id
+  let found = users.find((element) => {
+    return element._id == id
+  })
+  res.json(found)
+ })
 
+ // the POST Method
+ app.post('/users', (req, res) => {
+  console.log(req.body)
+  let newId = users.length + 1
+  req.body._id = newId
+  users.push(req.body)
+  res.json(users)
+ })
+
+ // the PUT Method
+//  app.put()
+
+ // the DELETE Method
+//  app.delete()
 /* END - create routes here */
 
 app.listen(port, () => 
