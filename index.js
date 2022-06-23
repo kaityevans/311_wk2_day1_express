fded08d5cbe61c5cb9429866e04eee217eba9cd4
unknown <kaitlandevans@gmail.com>
2022-06-23 18:37:04 -0500

    some put, delete in progress

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 // app.use(bodyParser())
-app.use(express.json())
-const port = process.env.PORT || 4000
+app.use(express.json());
+const port = process.env.PORT || 4000;
 
-const { users } = require('./state')
+const { users } = require("./state");
 
 /* BEGIN - create routes here */
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to my server</h1>')
-})
-app.get('/users', (req, res) => {
-  res.json(users)
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to my server</h1>");
+});
+app.get("/users", (req, res) => {
+  res.json(users);
+});
 
- // the GET Method
- app.get('/users/:id', (req, res) => {
-  console.log(req)
-  let id = req.params.id
+// the GET Method
+app.get("/users/:id", (req, res) => {
+  console.log(req);
+  let id = req.params.id;
   let found = users.find((element) => {
-    return element._id == id
-  })
-  res.json(found)
- })
+    return element._id == id;
+  });
+  res.json(found);
+});
 
- // the POST Method
- app.post('/users', (req, res) => {
-  console.log(req.body)
-  let newId = users.length + 1
-  req.body._id = newId
-  users.push(req.body)
-  res.json(users)
- })
+// the POST Method
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  let newId = users.length + 1;
+  req.body._id = newId;
+  users.push(req.body);
+  res.json(users);
+});
 
- // the PUT Method
-//  app.put()
+// the PUT Method
+app.put("/users/:id", (req, res) => {
+  const newPerson = {
+    ...person,
+    ...req.body,
+  }
+  console.log(person);
+  console.log(newPerson)
+});
 
- // the DELETE Method
-//  app.delete()
+// the DELETE Method
+ app.delete('users/:id', (req, res) => {
+//   let id = req.params.id
+//   let found = users.find((element) => {
+//     return element._id == id
+//   })
+//   res.json(found)
+//  })
 /* END - create routes here */
 
-app.listen(port, () => 
-  console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
